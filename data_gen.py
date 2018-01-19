5265d51d87746b1f8862433d561325204084c9b0
burklight <borjabrg12@gmail.com>
2018-01-19 16:06:25 +0100

    Add files via upload

--- data_gen.py
+++ data_gen.py
@@ -12,38 +12,46 @@ from collections import deque
 from numpy import linalg
 dirData = 'Data/'
 
-def Sinc1(N, x0, xN, sigma, norm = True):
-    eps = 1e-30
+eps = 1e-30
+
+def Sinc1(N, x0, xN, sigma):
     x = np.linspace(x0, xN, num=N)
     y = np.sin(x) / (x + eps) + np.random.normal(loc = 0, scale = sigma, size = N)
-    if norm == True:
-        y = y / np.abs(np.max(y) + eps)
     return x, y
 
 def genSinc1(norm = True):
+    eps = 1e-30
     N_tr = 100
     N_tst = 1000
-    X_tr, Y_tr = Sinc2(N_tr, -10, 10, 0.1, norm)
-    X_ts, Y_ts = Sinc2(N_tst, -10, 10, 0.1, norm)
+    X_tr, Y_tr = Sinc1(N_tr, -10, 10, 0.1)
+    X_ts, Y_ts = Sinc1(N_tst, -10, 10, 0.1)
+    if norm == True:
+        mean = np.mean(X_tr)
+        std = np.std(X_tr)
+        X_tr = (X_tr - mean) / (2 * std + eps)
+        X_ts = (X_ts - mean) / (2 * std + eps)
     return X_tr, Y_tr, X_ts, Y_ts
 
-def Sinc2(N, x0, xN, n0, nN, norm = True):
+def Sinc2(N, x0, xN, n0, nN):
     eps = 1e-30
     x = np.linspace(x0, xN, num=N)
     y = np.sin(x) / (x + eps) + np.random.uniform(low = n0, high = nN, size = N)
-    if norm == True:
-        y = y / np.abs(np.max(y) + eps)
     return x, y
 
 def genSinc2(norm = True):
+    eps = 1e-30
     N_tr = 100
     N_tst = 1000
-    X_tr, Y_tr = Sinc2(N_tr, -10, 10, -0.1, 0.1, norm )
-    X_ts, Y_ts = Sinc2(N_tst, -10, 10, -0.1, 0.1, norm)
+    X_tr, Y_tr = Sinc2(N_tr, -10, 10, -0.1, 0.1 )
+    X_ts, Y_ts = Sinc2(N_tst, -10, 10, -0.1, 0.1)
+    if norm == True:
+        mean = np.mean(X_tr)
+        std = np.std(X_tr)
+        X_tr = (X_tr - mean) / (2 * std + eps)
+        X_ts = (X_ts - mean) / (2 * std + eps)
     return X_tr, Y_tr, X_ts, Y_ts
 
-def Friedman1(N, norm = True):
-    eps = 1e-30
+def Friedman1(N):
     x1 = x2 = x3 = x4 = x5 = np.linspace(0, 1, num = N)
     y = 10*np.sin(np.pi*x1*x2) + 20*(x3 - 0.5)**2 \
         + 10*x4 + 5*x5
@@ -55,19 +63,22 @@ def Friedman1(N, norm = True):
     x[:,4] = x5
     sigma = np.sqrt(np.var(y)) / 3
     y = y + np.random.normal(loc = 0, scale = sigma, size = N)
-    if norm == True:
-        y = y / np.abs(np.max(y) + eps)
     return x,y
 
 def genFriedman1(norm = True):
+    eps = 1e-30
     N_tr = 240
     N_tst = 1000
-    X_tr, Y_tr = Friedman1(N_tr, norm)
-    X_ts, Y_ts = Friedman1(N_tst, norm)
+    X_tr, Y_tr = Friedman1(N_tr)
+    X_ts, Y_ts = Friedman1(N_tst)
+    if norm == True:
+        mean = np.mean(X_tr)
+        std = np.std(X_tr)
+        X_tr = (X_tr - mean) / (2 * std + eps)
+        X_ts = (X_ts - mean) / (2 * std + eps)
     return X_tr, Y_tr, X_ts, Y_ts
 
-def Friedman2(N, norm = True):
-    eps = 1e-30
+def Friedman2(N):
     x1 = np.linspace(0, 100, num = N)
     x2 = np.linspace(40*np.pi, 560*np.pi, num = N)
     x3 = np.linspace(0, 1, num = N)
@@ -81,19 +92,22 @@ def Friedman2(N, norm = True):
     x[:,3] = x4
     sigma = np.sqrt(np.var(y)) / 3
     y = y + np.random.normal(loc = 0, scale = sigma, size = N)
-    if norm == True:
-        y = y / np.abs(np.max(y) + eps)
     return x,y
 
 def genFriedman2(norm = True):
+    eps = 1e-30
     N_tr = 240
     N_tst = 1000
-    X_tr, Y_tr = Friedman2(N_tr, norm)
-    X_ts, Y_ts = Friedman2(N_tst, norm)
+    X_tr, Y_tr = Friedman2(N_tr)
+    X_ts, Y_ts = Friedman2(N_tst)
+    if norm == True:
+        mean = np.mean(X_tr)
+        std = np.std(X_tr)
+        X_tr = (X_tr - mean) / (2 * std + eps)
+        X_ts = (X_ts - mean) / (2 * std + eps)
     return X_tr, Y_tr, X_ts, Y_ts
 
-def Friedman3(N, norm):
-    eps = 1e-30
+def Friedman3(N):
     x1 = np.linspace(0, 100, num = N)
     x2 = np.linspace(40*np.pi, 560*np.pi, num = N)
     x3 = np.linspace(0, 1, num = N)
@@ -107,15 +121,19 @@ def Friedman3(N, norm):
     x[:,3] = x4
     sigma = np.sqrt(np.var(y)) / 3
     y = y + np.random.normal(loc = 0, scale = sigma, size = N)
-    if norm == True:
-        y = y / np.abs(np.max(y) + eps)
     return x,y
 
 def genFriedman3(norm = True):
+    eps = 1e-30
     N_tr = 240
     N_tst = 1000
-    X_tr, Y_tr = Friedman3(N_tr, norm)
-    X_ts, Y_ts = Friedman3(N_tst, norm)
+    X_tr, Y_tr = Friedman3(N_tr)
+    X_ts, Y_ts = Friedman3(N_tst)
+    if norm == True:
+        mean = np.mean(X_tr)
+        std = np.std(X_tr)
+        X_tr = (X_tr - mean) / (2 * std + eps)
+        X_ts = (X_ts - mean) / (2 * std + eps)
     return X_tr, Y_tr, X_ts, Y_ts
 
 def genBoston(norm = True):
@@ -133,26 +151,31 @@ def genBoston(norm = True):
     X_tr = x[0:481]
     Y_tr = y[0:481]
     X_ts = x[481:N]
-    Y_tx = x[481:N]
+    Y_ts = y[481:N]
     #X_tr = x[0:10]
     #Y_tr = y[0:10]
     #X_ts = x[300:N]
     #Y_ts = y[300:N]
+    if norm == True:
+        mean = np.mean(X_tr)
+        std = np.std(X_tr)
+        X_tr = (X_tr - mean) / (2 * std + eps)
+        X_ts = (X_ts - mean) / (2 * std + eps)
     return X_tr, Y_tr, X_ts, Y_ts
 
-def genData(dataset = "Sinc1"):
+def genData(dataset = "Sinc1", norm = False):
     if dataset == "Sinc1":
-        return genSinc1()
+        return genSinc1(norm = norm)
     elif dataset == "Sinc2":
-        return genSinc2()
+        return genSinc2(norm = norm)
     elif dataset == "F1":
-        return genFriedman1()
+        return genFriedman1(norm = norm)
     elif dataset == "F2":
-        return genFriedman2()
+        return genFriedman2(norm = norm)
     elif dataset == "F3":
-        return genFriedman3()
+        return genFriedman3(norm = norm)
     elif dataset == "Boston":
-        return genBoston()
+        return genBoston(norm = norm)
     else:
         print("Possible datasets: \n" + \
                   + "\t Sinc1" + \
